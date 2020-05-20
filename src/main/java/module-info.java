4676807d81367ec770e3ec382ecf670751e688ab
Matt Smith <matt@offtopica.uk>
2020-05-20 15:11:32 +0100

    Remove runtime lombok dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,12 @@ open module uk.offtopica.monerorpc {
     exports uk.offtopica.monerorpc.daemon;
     exports uk.offtopica.monerorpc.wallet;
 
+    requires static lombok;
+
     requires java.net.http;
 
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
-    requires lombok;
     requires org.slf4j;
 }
