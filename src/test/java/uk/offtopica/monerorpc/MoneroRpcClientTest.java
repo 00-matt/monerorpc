f3c3b5054f292a33eb0540cd06a03df27bf1057f
Matt Smith <matt@offtopica.uk>
2020-07-27 12:17:54 +0100

    Rewrite tests to use MockServer

--- src/test/java/uk/offtopica/monerorpc/MoneroRpcClientTest.java
+++ src/test/java/uk/offtopica/monerorpc/MoneroRpcClientTest.java
@@ -1,28 +1,90 @@
 package uk.offtopica.monerorpc;
 
-import com.pgssoft.httpclient.HttpClientMock;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.junit.jupiter.MockServerExtension;
+import org.mockserver.matchers.MatchType;
 
-import java.lang.reflect.Field;
 import java.net.URI;
+import java.util.Map;
 
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.model.JsonBody.json;
+
+@ExtendWith(MockServerExtension.class)
 public abstract class MoneroRpcClientTest<T extends MoneroRpcClient> {
-    protected final URI uri;
     protected T client;
-    protected HttpClientMock http;
+    protected MockServerClient http;
+    private final ObjectMapper objectMapper;
 
     public MoneroRpcClientTest() {
-        uri = URI.create("http://localhost:18081/json_rpc");
+        objectMapper = new ObjectMapper();
     }
 
     @BeforeEach
-    void setupMoneroRpcClientTest() throws Exception {
-        client = createClient(uri);
-        http = new HttpClientMock();
+    void setupMoneroRpcClientTest(MockServerClient http) {
+        this.http = http;
+        http.reset();
+        client = createClient(URI.create(String.format("http://localhost:%d/json_rpc", http.getPort())));
+    }
 
-        final Field httpClientField = MoneroRpcClient.class.getDeclaredField("httpClient");
-        httpClientField.setAccessible(true);
-        httpClientField.set(client, http);
+    protected void setupMockEndpoint(String method, Object expectedRequestParams, Object response) {
+        http.when(
+                request()
+                        .withMethod("POST")
+                        .withPath("/json_rpc")
+                        .withBody(json(
+                                Map.of(
+                                        "method", method,
+                                        "params", expectedRequestParams
+                                ),
+                                MatchType.ONLY_MATCHING_FIELDS
+                        ))
+        ).respond(
+                response()
+                        .withBody(responseJson(response))
+        );
+    }
+
+    protected void setupMockEndpointSuccess(String method, Object expectedRequestParams, Object result) {
+        setupMockEndpoint(
+                method,
+                expectedRequestParams,
+                Map.of(
+                        "id", 0,
+                        "jsonrpc", "2.0",
+                        "result", result
+                )
+        );
+    }
+
+    protected void setupMockEndpointError(String method, Object expectedRequestParams, int errorCode,
+                                          String errorMessage) {
+        setupMockEndpoint(
+                method,
+                expectedRequestParams,
+                Map.of(
+                        "id", 0,
+                        "jsonrpc", "2.0",
+                        "error", Map.of(
+                                "code", errorCode,
+                                "message", errorMessage
+                        )
+                )
+        );
+    }
+
+    private String responseJson(Object object) {
+        // The MockClient json() method removes empty strings and objects :(
+        try {
+            return objectMapper.writer().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     protected abstract T createClient(URI uri);
--- src/test/java/uk/offtopica/monerorpc/wallet/MoneroWalletRpcClientTest.java
+++ src/test/java/uk/offtopica/monerorpc/wallet/MoneroWalletRpcClientTest.java
@@ -5,6 +5,7 @@ import uk.offtopica.monerorpc.MoneroRpcClientTest;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.offtopica.monerorpc.utils.HexUtils.hexStringToByteArray;
@@ -12,15 +13,17 @@ import static uk.offtopica.monerorpc.utils.HexUtils.hexStringToByteArray;
 class MoneroWalletRpcClientTest extends MoneroRpcClientTest<MoneroWalletRpcClient> {
     @Test
     void testCreateAddressSuccess() throws Exception {
-        http.onPost((uri.toString()))
-                .doReturnJSON("{\n" +
-                        "  \"id\": \"0\",\n" +
-                        "  \"jsonrpc\": \"2.0\",\n" +
-                        "  \"result\": {\n" +
-                        "    \"address\": \"7BG5jr9QS5sGMdpbBrZEwVLZjSKJGJBsXdZLt8wiXyhhLjy7x2LZxsrAnHTgD8oG46ZtLjUGic2pWc96GFkGNPQQDA3Dt7Q\",\n" +
-                        "    \"address_index\": 5\n" +
-                        "  }\n" +
-                        "}");
+        setupMockEndpointSuccess(
+                "create_address",
+                Map.of(
+                        "account_index", 0
+                ),
+                Map.of(
+                        "address_index", 5,
+                        "address",
+                        "7BG5jr9QS5sGMdpbBrZEwVLZjSKJGJBsXdZLt8wiXyhhLjy7x2LZxsrAnHTgD8oG46ZtLjUGic2pWc96GFkGNPQQDA3Dt7Q"
+                )
+        );
 
         assertEquals(new IndexedAddress(
                         0,
@@ -32,23 +35,44 @@ class MoneroWalletRpcClientTest extends MoneroRpcClientTest<MoneroWalletRpcClien
 
     @Test
     void testTransferSuccessOneResult() throws Exception {
-        http.onPost(uri.toString())
-                .doReturnJSON("{\n" +
-                        "  \"id\": \"0\",\n" +
-                        "  \"jsonrpc\": \"2.0\",\n" +
-                        "  \"result\": {\n" +
-                        "    \"amount_list\": [3000000000000],\n" +
-                        "    \"fee_list\": [473710000],\n" +
-                        "    \"multisig_txset\": \"\",\n" +
-                        "    \"tx_hash_list\": [\"4adcdc1af3f665770cdf8fb7a380887cd07ac53c2b771bd18df5ca375d5e7540" +
-                        "\"],\n" +
-                        "    \"tx_key_list\": " +
-                        "[\"5b455c0f97168be652a2c03c5c68a064bb84cdae4ddef01b5c48d73a0bbb27075fb714f2ca19ea6c8ff592" +
-                        "417e606addea6deb1d6530e2969f75681ffcbfc4075677b94a8c9197963ae38fa6f543ee68f0a4c4bbda4c453" +
-                        "f39538f00b28e980ea08509730b51c004960101ba2f3adbc34cbbdff0d5af9dba061b523090debd06\"],\n" +
-                        "    \"unsigned_txset\": \"\"\n" +
-                        "  }\n" +
-                        "}");
+        setupMockEndpointSuccess(
+                "transfer_split",
+                Map.of(
+                        "destinations",
+                        List.of(
+                                Map.of(
+                                        "address",
+                                        "7BnERTpvL5MbCLtj5n9No7J5oE5hHiB3tVCK5cjSvCsYWD2WRJLFuWeKTLiXo5QJqt2ZwUaLy2Vh" +
+                                                "1Ad51K7FNgqcHgjW85o",
+                                        "amount",
+                                        1000000000000L
+                                ),
+                                Map.of(
+                                        "address",
+                                        "75sNpRwUtekcJGejMuLSGA71QFuK1qcCVLZnYRTfQLgFU5nJ7xiAHtR5ihioS53KMe8pBhH61mor" +
+                                                "aZHyLoG4G7fMER8xkNv",
+                                        "amount",
+                                        2000000000000L
+                                )
+                        )
+                ),
+                Map.of(
+                        "amount_list",
+                        List.of(3000000000000L),
+                        "fee_list",
+                        List.of(473710000L),
+                        "multisig_txset",
+                        "",
+                        "tx_hash_list",
+                        List.of("4adcdc1af3f665770cdf8fb7a380887cd07ac53c2b771bd18df5ca375d5e7540"),
+                        "tx_key_list",
+                        List.of("5b455c0f97168be652a2c03c5c68a064bb84cdae4ddef01b5c48d73a0bbb27075fb714f2ca19ea6c8ff5" +
+                                "92417e606addea6deb1d6530e2969f75681ffcbfc4075677b94a8c9197963ae38fa6f543ee68f0a4c4bb" +
+                                "da4c453f39538f00b28e980ea08509730b51c004960101ba2f3adbc34cbbdff0d5af9dba061b523090debd06"),
+                        "unsigned_txset",
+                        ""
+                )
+        );
 
         assertEquals(List.of(new TransferResult(
                 hexStringToByteArray("4adcdc1af3f665770cdf8fb7a380887cd07ac53c2b771bd18df5ca375d5e7540"),
